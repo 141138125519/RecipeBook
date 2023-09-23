672812472890f2460b8982721960b8f2da93ce79
Adam <adam-c-hayes@hotmail.co.uk>
2023-09-23 16:00:25 +0100

    Change Cooking time to int

---
+++ Migrations/20230923144909_cooking_time_to_int.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeBook.Migrations
+{
+    /// <inheritdoc />
+    public partial class cooking_time_to_int : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CookingTime",
+                table: "Recipes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CookingTimeMins",
+                table: "Recipes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CookingTimeMins",
+                table: "Recipes");
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "CookingTime",
+                table: "Recipes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+        }
+    }
+}
