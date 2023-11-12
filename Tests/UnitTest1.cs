63600847cf680e128a8545752ed8251cae5508c5
Adam <adam-c-hayes@hotmail.co.uk>
2023-11-12 18:37:08 +0000

    update project structure

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        // Ensure the tests will run.
+        public void TestTest()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
